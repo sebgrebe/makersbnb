d536e3ea142a6d1701bfda85ffba22d0087bb4b3
Sebastian Becker <seb@Sebastians-MacBook-Pro.local>
2018-10-09 15:00:47 +0100

    booking on frontend

--- public/src/rooms.js
+++ public/src/rooms.js
@@ -36,10 +36,32 @@ $(document).ready(function() {
   $( ".booking-button" ).click(function(val) {
     var button_id = val.currentTarget.id
     var room_id = button_id.substr(15,button_id.length-1)
-    //TODO: input ajax call to book room
-    alert("You booked room " + room_id)
+    //bookRoom(room_id)
+    showBookingConfirmation('studioflat') //this needs to be deleted once ajax is working
   });
+
+  $( ".booking-button-disabled" ).click(function() {
+    console.log('disabled')
+    showBookingError()
+  })
 })
+
+function bookRoom(room_id){
+   $.ajax({
+    url: 'http://localhost:9292/api/bookroom',
+    type: 'POST',
+    dataType: 'json',
+    body: {room_id: room_id}
+
+  })
+  .done(function(data) {
+    // implement logic for checking the response.
+  })
+  .fail(function(xhr,status,errorThrown) {
+    alert("Sorry, there was a problem. Status: " + status)
+  })
+
+}
 
 function formatAvailability(availability) {
   if(availability === true) {
@@ -51,21 +73,31 @@ function formatAvailability(availability) {
 
 function getRooms() {
   // TODO: uncomment when database API is working
-  $.ajax({
-    url: 'http://localhost:9292/api/rooms',
-    type: 'GET',
-    dataType: 'json'
-  })
-  .done(function(data) {
-    console.log(data)
-   // data = JSON.parse(data_mock_json);
-   // return data;
-  })
-  .fail(function(xhr,status,errorThrown) {
-    alert("Sorry, there was a problem. Status: " + status)
-  })
-  // data = JSON.parse(data_mock_json);
-  // return data;
+  // $.ajax({
+  //   url: 'http://localhost:9292/api/rooms',
+  //   type: 'GET',
+  //   dataType: 'json'
+  // })
+  // .done(function(data) {
+  //   console.log(data)
+  //  // data = JSON.parse(data_mock_json);
+  //  // return data;
+  // })
+  // .fail(function(xhr,status,errorThrown) {
+  //   alert("Sorry, there was a problem. Status: " + status)
+  // })
+  data = JSON.parse(data_mock_json);
+  return data;
+}
+
+function showBookingConfirmation(room_name){
+  $('#messages').text('')
+  $('#messages').append('<div class="booking-confirmation">You have successfully booked the room: ' + room_name + '</div>');
+}
+
+function showBookingError() {
+  $('#messages').text('')
+  $('#messages').append('<div class="booking-error">You cannot book unavailable rooms</div>');
 }
 
 function roomHTML(room) {
@@ -76,12 +108,14 @@ function roomHTML(room) {
   room_div += "<div class ='room-price'> Price per night: £" + room.price + "</div>"
   room_div += "<div class ='room-available'> Available: " + formatAvailability(room.available) + "</div>"
   room_div += "<div class ='room-owner'> Owner: " + room.owner + "</div>"
-  room_div += bookingButtonHTML(room.id)
+  room_div += bookingButtonHTML(room.id,room.available)
   room_div += "</div>"
   return room_div
 }
 
-function bookingButtonHTML(id) {
-  var booking_button_div = "<button id='booking-button-" + id + "' class='booking-button'> Book </button>"
+function bookingButtonHTML(id,availability) {
+  var booking_button_div = ''
+  if (availability) { booking_button_div = "<button id='booking-button-" + id + "' class='booking-button'> Book </button>" }
+  else { booking_button_div = "<button class='booking-button-disabled'> Book </button>" }
   return booking_button_div
 }
