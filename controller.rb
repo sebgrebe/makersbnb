1784c98285c7156217ec1a457114e168b005a7a9
Sebastian Becker <seb@Sebastians-MacBook-Pro.local>
2018-10-10 17:07:52 +0100

    signup frontend

--- controller.rb
+++ controller.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require_relative 'lib/rooms'
+require_relative 'lib/users'
 
 class MakersBnB < Sinatra::Base
 
@@ -13,8 +14,17 @@ class MakersBnB < Sinatra::Base
     File.read(File.join('public', 'rooms.html'))
   end
 
+  get '/users/signup' do
+    File.read(File.join('public', 'signup.html'))
+  end
+
   get '/api/rooms' do
      Rooms.list_rooms.to_json
+  end
+
+  post '/api/signup' do
+    signup = params[:signup]
+    Users.sign_up(signup['email'], signup['password'], signup['first_name'], signup['last_name']).to_json
   end
 
   post '/api/book_room' do
@@ -25,6 +35,7 @@ class MakersBnB < Sinatra::Base
     @offer = params[:offer]
     Rooms.add_room(@offer).to_json
   end
+
 
   run! if app_file == $0
 
