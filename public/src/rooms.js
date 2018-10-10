0ba9865cab1bc764ce9653adfb183fe9b775e66c
ChloeHelen <cmingay@fastmail.fm>
2018-10-10 16:05:05 +0100

    dynamically changing status

--- public/src/rooms.js
+++ public/src/rooms.js
@@ -23,7 +23,10 @@ function bookRoom(room_id){
   })
   .done(function(data) {
     console.log(data)
-    if (data.booked) { showBookingConfirmation(data.room[0]['room_name'])}
+    if (data.booked) { showBookingConfirmation(data.room[0]['room_name'])
+      changeAvailability(data.room[0]['room_id']);
+  }
+    else{ showBookingError();}
   })
   .fail(function(xhr,status,errorThrown) {
     alert("Sorry, there was a problem. Status: " + status)
@@ -64,13 +67,17 @@ function showBookingError() {
   $('#messages').append('<div class="error">You cannot book unavailable rooms</div>');
 }
 
+function changeAvailability(room_id){
+  $('.room-available'+room_id).text('Available: ' + formatAvailability('f'));
+}
+
 function roomHTML(room) {
   var room_div = "<div id='room-" + room.room_id + "' class='room'>"
   room_div += "<div class='room-name'>" + room.room_name + "</div>"
   room_div += "<div class ='room-desc'>" + room.description + "</div>"
   room_div += "<div class ='room-location'>" + room.location + "</div>"
   room_div += "<div class ='room-price'> Price per night: £" + room.price_per_night + "</div>"
-  room_div += "<div class ='room-available'> Available: " + formatAvailability(room.available) + "</div>"
+  room_div += "<div class ='room-available" + room.room_id + "'> Available: " + formatAvailability(room.available) + "</div>"
   room_div += "<div class ='room-owner'> Owner: " + room.owner_user_id + "</div>"
   room_div += bookingButtonHTML(room.room_id,room.available)
   room_div += "</div>"
